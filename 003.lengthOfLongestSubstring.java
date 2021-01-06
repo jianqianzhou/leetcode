1bcc62cb345b5da5cf05753dab643c80a491639f
jianqianzhou <jianqianzhou@trip.com>
2021-01-06 10:34:49 +0800

    滑动窗口

--- 003.lengthOfLongestSubstring.java
+++ 003.lengthOfLongestSubstring.java
@@ -1,19 +1,19 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-           Map<Character,Integer> win = new HashMap<>();
+           Map<Character,Integer> win = new HashMap<>(); //定义窗口，窗口中的字符串必须是不重复的
            int left = 0, right = 0, res =0;
            while(right<s.length()){
                char ch = s.charAt(right);
                right++;
                win.put(ch, win.getOrDefault(ch,0)+1);
-               while(win.get(ch)>1){
+               while(win.get(ch)>1){  //当窗口中的字符串出现重复的话
                    char d = s.charAt(left);
-                   left++;
+                   left++;    //窗口往左移
                    int temp = win.get(d);
                    temp--;
                    win.put(d,temp);
                }
-               res = Math.max(res,right-left);
+               res = Math.max(res,right-left); //去窗口的最大值
            }
            return res;
     }
