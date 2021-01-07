ebdfe980f1051a83085df3f7e5176c744a81502a
jianqianzhou <62974595+jianqianzhou@users.noreply.github.com>
2021-01-07 21:33:36 +0800

    双指针
    双指针

--- 011.maxArea.java
+++ 011.maxArea.java
@@ -14,3 +14,21 @@ class Solution {
 
     }
 }
+
+
+
+//////////////////////////
+public int maxArea(int[] height) {
+    int l = 0;
+    int r = height.length-1;
+    while(l<r){
+        int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+    }
+}
