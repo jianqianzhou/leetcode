ff08e54467312a5bb89a27c7bbadc07dc8482c4b
jianqianzhou <jianqianzhou@trip.com>
2021-01-10 16:19:25 +0800

    双指针

---
+++ 013.romanToInt.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int romanToInt(String s) {
+       Map<Character, Integer> map = new HashMap<>();
+           map.put('I',1);
+           map.put('V',5);
+           map.put('X',10);
+           map.put('L', 50);
+           map.put('C',100);
+           map.put('D',500);
+           map.put('M',1000);
+           int res = 0;
+           int i=0;
+           int n = s.length();
+           while(i<n){
+               char ch = s.charAt(i);
+               
+               if(ch=='I' && i+1<n && s.charAt(i+1)=='V'){ res += 4;i++;}
+               else if(ch=='I' && i+1<n && s.charAt(i+1)=='X') {res += 9;i++;}
+               else if(ch=='X'  && i+1<n && s.charAt(i+1)=='L'){ res += 40;i++;}
+               else if(ch=='X'  && i+1<n && s.charAt(i+1)=='C') {res += 90;i++;}
+               else if(ch=='C' && i+1<n && s.charAt(i+1)=='D'){ res += 400;i++;}
+               else if(ch=='C' && i+1<n && s.charAt(i+1)=='M' ) {res+= 900;i++;}
+               else{
+                  if(i<n){
+                       ch = s.charAt(i);
+                       res += map.get(ch);
+                  }
+               }
+               i++;
+          }
+          return res;
+    }
+ 
+   ///////////////////////////////////////////
+   public int romanToInt(String s) {
+   }
+}
