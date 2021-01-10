b957516879ca8e04aea07d6e3ba53afad01a36ca
jianqianzhou <jianqianzhou@trip.com>
2021-01-10 16:43:35 +0800

    字符串处理

--- 013.romanToInt.java
+++ 013.romanToInt.java
@@ -32,6 +32,29 @@ class Solution {
     }
  
    ///////////////////////////////////////////
+   //把一个小值放在大值的左边，就是做减法，否则为加法。小数据集switch case时间复杂度高于map
    public int romanToInt(String s) {
+         int sum = 0;
+         int a = getValue(s.charAt(0));
+         for(int i=1; i<s.length(); i++){
+              int b =  getValue(s.charAt(i));
+              if(a<b){ sum -= a;}
+              else{sum +=a;}
+              a = b;
+         }
+         sum += a;
+         return sum;
+   }
+   public int getValue(char ch){
+        switch(ch){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default :return 0;
+        }
    }
 }
