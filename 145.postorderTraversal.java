d03ce7d2dfc66d69c699925b42d50aaf1990f894
jianqianzhou <jianqianzhou@trip.com>
2021-01-28 11:27:32 +0800

    前中后遍历

--- 145.postorderTraversal.java
+++ 145.postorderTraversal.java
@@ -1,5 +1,5 @@
 class Solution {
-    /////////////////////////////非递归
+    /////////////////////////////非递归 后序遍历  左右根
     public List<Integer> postorderTraversal(TreeNode root) {
            List<Integer> ans = new ArrayList<Integer>();
            if(root==null) return ans;
@@ -24,6 +24,43 @@ class Solution {
            return ans;
     }
  
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root==null) return res;
+        Stack<TreeNode> queue = new Stack<>();
+        TreeNode cur = root;
+        while(cur!= null || !queue.isEmpty()){
+              while(cur!=null){
+                  res.add(cur.val);   //加入到集合中
+                  queue.push(cur);
+                  cur = cur.left;
+              }
+
+              cur = queue.pop();
+              cur = cur.right;
+        }
+        return res;
+    
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+             List<Integer> ans = new ArrayList<>();
+             if(root==null) return ans;
+             Stack<TreeNode> queue = new Stack<>();  //使用Stack，不能使用LinkedList
+             TreeNode cur = root;
+             while(cur != null || !queue.isEmpty()){  //这两个判断条件非常重要，当cur为null时，不能进入循环
+                  while(cur!=null){
+                       queue.push(cur);
+                       cur = cur.left;
+                  }
+                  cur = stack.pop();
+                  ans.add(cur.val);    //加入到集合中
+                  cur = cur.right;
+             }
+             return ans;
+       }
+    
+
     ///////////////////////////////递归方式
     List<Integer> ans = new ArrayList<Integer>();
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -33,4 +70,6 @@ class Solution {
           ans.add(root.val);
           return ans;
     }
+
+   
 }
