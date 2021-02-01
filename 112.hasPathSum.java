d8c12110ad4e240afc0d44ac8b4b273a786663eb
jianqianzhou <62974595+jianqianzhou@users.noreply.github.com>
2021-02-01 21:40:10 +0800

    Update 112.hasPathSum.java

--- 112.hasPathSum.java
+++ 112.hasPathSum.java
@@ -5,3 +5,25 @@ public boolean hasPathSum(TreeNode root, int targetSum) {  //递归遍历
              return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val); //判断左节点和右节点
     }
     }
+
+//非递归遍历
+ public boolean hasPathSum(TreeNode root, int targetSum) {   
+           if(root==null) return false;
+           LinkedList<TreeNode> queue = new LinkedList<>();   //简单的层序遍历方式
+           LinkedList<Integer> q_val = new LinkedList<>();   //多添加一个队列用来存放树计算过程中的累加和，最后生成的树是一个累加和树
+           queue.add(root);
+           q_val.offer(root.val);
+           while(!queue.isEmpty()){
+                 int size = queue.size();
+                 for(int i=0; i<size; i++){
+                       TreeNode node = queue.poll();
+                       int temp = q_val.poll();
+                       if(temp==targetSum && node.left==null && node.right==null) return true;
+                       if(node.left!=null){
+                           queue.offer(node.left); q_val.offer(temp+node.left.val);}  //将当前节点的值和左节点的值相加放入队列中
+                       if(node.right!=null){
+                           queue.offer(node.right); q_val.offer(temp+node.right.val);}
+                 }
+           }
+           return false;
+    }
