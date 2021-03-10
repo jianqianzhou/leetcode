196e7c837cb6ad7002db456efd1bc0efcff2d57b
jianqianzhou <jianqianzhou@trip.com>
2021-03-10 19:33:26 +0800

    四数相加

---
+++ 454.fourSumCount.java
@@ -0,0 +1,19 @@
+class Solution {
+       public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+           Map<Integer,Integer> map = new HashMap<>();
+           for(int a:A){
+              for(int b:B){
+                  map.put(a+b, map.getOrDefault(a+b,0)+1);
+              }
+           }
+           int count = 0;
+           for(int c:C){
+              for(int d: D){
+                   if(map.containsKey(-c-d)){
+                        count += map.get(-c-d);
+                   }
+              }
+           }
+           return count;
+    }
+}
